a02a3136754b416d1a5370c3d16b298a1b3f7898
SurajK88888 <surajk961612@gmail.com>
2026-02-19 20:52:37 +0530

    feat: add POST /students API with JSON middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 import express from "express";
 
-const app = express()
+const app = express();
 const PORT = 5000;
 
-app.get("/",(req,res)=>{
-    res.send("Student Management SaaS Backend is running");
+// This is a middleware to parse json to js object and attach data to req.body
+app.use(express.json());
+
+// endpoint // API
+app.get("/", (req, res) => {
+  //   const data = req.body;
+  //   console.log(data.name);
+  res.send("Student Management SaaS Backend is running");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
+//endpoint // API
+app.post("/students", (req, res) => {
+  const student = req.body;
+  console.log("Received Student: ", student);
+
+  res.status(200).json({
+    message: "Student created successfully.",
+    data: student,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
