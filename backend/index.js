0a131fce77275aa663d373177d9a810f326e9e99
SurajK88888 <surajk961612@gmail.com>
2026-02-20 23:15:42 +0530

    feat: implement in-memory student storage with GET and POST routes

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,31 @@ import express from "express";
 
 const app = express();
 const PORT = 5000;
+let students = [];
 
 // This is a middleware to parse json to js object and attach data to req.body
 app.use(express.json());
 
 // endpoint // API
 app.get("/", (req, res) => {
-  //   const data = req.body;
-  //   console.log(data.name);
   res.send("Student Management SaaS Backend is running");
+});
+
+// endpoint //API
+app.get("/students", (req, res) => {
+  res.json({
+    count: students.length,
+    data: students,
+  });
 });
 
 //endpoint // API
 app.post("/students", (req, res) => {
   const student = req.body;
-  console.log("Received Student: ", student);
 
-  res.status(200).json({
+  students.push(student);
+
+  res.status(201).json({
     message: "Student created successfully.",
     data: student,
   });
